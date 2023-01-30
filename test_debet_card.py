930b392662b4599e58ab8e6f6e3c8b1b97aa7a72
itamitut <itamitut@yandex.ru>
2023-01-30 14:03:16 +0300

    Refactor debet card

--- test_debet_card.py
+++ test_debet_card.py
@@ -1,6 +1,5 @@
 import random
 import time
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -58,23 +57,13 @@ for debet_card in debet_cards:
     driver.get(url + debet_card)
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[@class="button--g31Xx card-brief__button"]'))).click()
 
-# Заполняем поля анкеты
-    elem = driver.find_element(By.XPATH,
-                               '//*[@id="root"]/div[1]/div[3]/div/div/div[1]/div[1]/div[2]/div/div[1]/div[1]/div/div/div/input')
-    choose_suggestion(elem, 'char')
-
-    elem = driver.find_element(By.XPATH,
-                               '//*[@id="root"]/div[1]/div[3]/div/div/div[1]/div[1]/div[2]/div/div[1]/div[2]/div/div/div/input')
-    choose_suggestion(elem, 'char')
-
-    elem = driver.find_element(By.XPATH,
-                               '//*[@id="root"]/div[1]/div[3]/div/div/div[1]/div[1]/div[2]/div/div[2]/div[1]/div[1]/div/div/input')
-    choose_suggestion(elem, 'char')
+    # Заполняем поля анкеты
+    fio = driver.find_elements(By.XPATH, '//input[@class="smartInput__input--zqFgL"]')
+    for i in range(3):
+        choose_suggestion(fio[i], 'char')
     # выбираем пол:
     sex = driver.find_elements(By.XPATH, '//input[@class="radio__optionInput--fSYTn"]')
-
     random.choice(sex).click()
-
     # Дата рождения
     elem = driver.find_element(By.XPATH,
                                '//*[@id="root"]/div[1]/div[3]/div/div/div[1]/div[1]/div[2]/div/div[2]/div[2]/div/div[1]')
@@ -82,7 +71,7 @@ for debet_card in debet_cards:
     elem = driver.find_element(By.XPATH,
                                '//*[@id="root"]/div[1]/div[3]/div/div/div[1]/div[1]/div[2]/div/div[2]/div[2]/div/div[1]/div/input')
     slow_input(elem, '11111980')
-
+    #Телефон
     elem = driver.find_element(By.XPATH,
                                '//*[@id="root"]/div[1]/div[3]/div/div/div[1]/div[1]/div[2]/div/div[4]/div[1]/div/div[1]/input')
     elem.click()
